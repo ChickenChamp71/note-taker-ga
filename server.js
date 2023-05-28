2dbbca30d5f738ff73793ef62b1f2dfdc5dd7148
ChickenChamp71 <chickensandsharks@gmail.com>
2023-05-28 05:20:40 -0700

    removing it bc it didn't work

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
