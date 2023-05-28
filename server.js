fddba0a5fa80afb3adca7ff66a8fd202d61c4737
ChickenChamp71 <chickensandsharks@gmail.com>
2023-05-28 05:14:03 -0700

    added * pathing

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', './public/index.html'))
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
